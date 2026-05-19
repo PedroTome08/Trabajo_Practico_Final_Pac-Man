1fb917d977cb3356f0f393d6c8624e241c8e6250
arianneseput <arianneseput@gmail.com>
2026-05-19 16:20:06 -0300

    Importé la librería pygame, creé la pantalla y el movimiento del packman.

---
+++ src/juego.py
@@ -0,0 +1,41 @@
+import sys
+import pygame
+
+sys.path.append("..")
+
+from models.pac_man import PacMan
+
+pygame.init()
+pantalla = pygame.display.set_mode((1280, 720))       
+reloj = pygame.time.Clock()
+corriendo = True
+dt = 0      #delta tiempo
+pacman = PacMan(x = pantalla.get_width() / 2, y = pantalla.get_height() / 2, vidas = 3, velocidad=100)
+
+while corriendo:
+    #acá se almacenan los movimientos
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            corriendo = False
+
+    #color de la pantalla
+    pantalla.fill("black") 
+
+    pacman.dibujar(pantalla)
+    
+    #que pasa cuando toco las teclas AWSD para moverlo
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        pacman.mover("arriba", dt)
+    if keys[pygame.K_s]:
+        pacman.mover("abajo", dt)
+    if keys[pygame.K_a]:
+        pacman.mover("izquierda", dt)
+    if keys[pygame.K_d]:
+        pacman.mover("derecha", dt)
+
+    pygame.display.flip()
+
+    dt = reloj.tick(60) / 1000
+
+pygame.quit()
---
+++ src/models/pac_man.py
@@ -0,0 +1,26 @@
+import pygame
+
+class PacMan:
+    def __init__ (self, x, y, vidas, velocidad):
+        self.x = x
+        self.y = y
+        self.vidas = vidas
+        self.velocidad = velocidad
+    
+    #función que dibuje al pacman
+    
+    def dibujar(self, pantalla):
+        pygame.draw.circle(pantalla, "yellow", pygame.Vector2(self.x, self.y), 20)
+
+    def mover(self, movimiento, dt):
+        if movimiento == "arriba":
+            self.y -= self.velocidad * dt
+        elif movimiento == 'abajo':
+            self.y += self.velocidad * dt
+        elif movimiento == 'derecha':
+            self.x += self.velocidad * dt
+        elif movimiento == 'izquierda':
+            self.x -= self.velocidad * dt
+
+    #metodo que le saca la vida
+    #hacer otra clase para fantasmas y otra para manejar el jueg(puntaje, mapa, jugador, fantasmas)
