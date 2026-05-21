d2e59391bbb38ad898d1e0d1bc5f7c23cd9aaa4d
arianneseput <arianneseput@gmail.com>
2026-05-21 16:07:27 -0300

    creé la clase para el menú

---
+++ src/models/menu.py
@@ -0,0 +1,11 @@
+#pantalla principal: high score \n el numero \n PAC - MAN
+#input de presionar ENTER para jugar (tiene que parpadear)
+#Elegí 4 fantasmas [n / 4]: lista con opciones
+#asignar una esquina a cada uno 
+#comienza el juego con las vidas abajo, con el score 
+
+
+
+class Menu:
+    def __init__(self):
+        self
